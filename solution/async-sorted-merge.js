741ce7b91faddf7bd1d8fb97dda8b69dc60a88d8
AlejoYarce <alejo.yarce990@gmail.com>
2022-10-20 14:16:08 -0500

    log sorting sync + async

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,24 @@
-"use strict";
+'use strict'
+
+const lodash = require('lodash')
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
-};
+  return new Promise(async (resolve) => {
+    logSources.sort((a, b) => a.last.date - b.last.date)
+
+    while (logSources.length > 0) {
+      const firstLogSource = logSources.shift()
+      printer.print(firstLogSource.last)
+
+      if (!firstLogSource.drained) {
+        await firstLogSource.popAsync()
+        logSources.splice(lodash.sortedIndexBy(logSources, firstLogSource, 'last.date'), 0, firstLogSource)
+      }
+    }
+
+    printer.done()
+    resolve(console.log('Async sort complete.'))
+  })
+}
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,22 @@
-"use strict";
+'use strict'
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+  logSources.sort((a, b) => a.last.date - b.last.date)
+
+  while (logSources.length > 0) {
+    const firstLogSource = logSources[0]
+    printer.print(firstLogSource.last)
+
+    if (!firstLogSource.drained) {
+      firstLogSource.pop()
+      logSources.sort((a, b) => a.last.date - b.last.date)
+    } else {
+      logSources.shift()
+    }
+  }
+
+  printer.done()
+  return console.log('Sync sort complete.')
+}
